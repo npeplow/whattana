859b6046335240098dd4e60a418cb9413a4fe296
Nick <nick@nicks-mbp.ibsglite.com>
2016-05-17 16:57:50 +0700

    check conn

--- phpinfo.php
+++ phpinfo.php
@@ -12,6 +12,13 @@ $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
 
 // Show all information, defaults to INFO_ALL
 phpinfo();
