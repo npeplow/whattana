1a8e98496e718dfc3723fa046052e4b66193555f
Nick <nick@nicks-mbp.ibsglite.com>
2016-05-17 16:56:22 +0700

    con

--- phpinfo.php
+++ phpinfo.php
@@ -1,6 +1,17 @@
 <?php
 
-echo $my_env_var = getenv('CLEARDB_DATABASE_URL');
+echo $my_env_var = getenv('JAWSDB_URL');
+
+
+$url = parse_url(getenv($my_env_var));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
 
 // Show all information, defaults to INFO_ALL
 phpinfo();
