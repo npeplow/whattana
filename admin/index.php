59601c65667363e169c519a410c840be8b3100ad
Nick <nick@nicks-mbp.ibsglite.com>
2016-05-17 16:17:17 +0700

    forward slash

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 //require "vendor/autoload.php";
 use CrudKit\CrudKitApp;
 use CrudKit\Pages\SQLiteTablePage;
